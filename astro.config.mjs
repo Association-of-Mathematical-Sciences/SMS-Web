c69dfdf4538628b7ed4b47d09ccebc60d580ed3c
Josh-Hiz <jhizgiaev2704@gmail.com>
2025-11-24 01:49:41 -0500

    deploy test

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Stevens-Mathematical-Club.github.io',
+    site: 'https://Stevens-Mathematical-Club.github.io/SMS-Web',
     base: '/SMS-Web',
 });
