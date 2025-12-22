4a42323ffd5a13d543fcfe3d9f81da6a4e9341e6
Josh-Hiz <jhizgiaev2704@gmail.com>
2025-12-22 15:49:12 -0500

    Remove SMS-Web base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Stevens-Mathematical-Club.github.io/SMS-Web',
-  base: '/SMS-Web',
   prefetch: {
     prefetchAll: true
   },
