53d40e022694426ba10460a71d348732f69c00bd
Josh-Hiz <jhizgiaev2704@gmail.com>
2025-12-22 19:20:00 -0500

    attempt to fix deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,16 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://Stevens-Mathematical-Club.github.io/SMS-Web',
-  base: '/SMS-Web',
-  prefetch: {
-    prefetchAll: true
-  },
-  vite: {
-      plugins: [tailwindcss()],
-      css: {
-        transformer: "lightningcss"
-      }
-  },
+	site: 'https://Stevens-Mathematical-Club.github.io/SMS-Web',
+	prefetch: {
+		prefetchAll: true,
+	},
+	vite: {
+		plugins: [tailwindcss()],
+		css: {
+			transformer: 'lightningcss',
+		},
+	},
 
-  integrations: [
-    (await import("@playform/compress")).default()
-  ]
+	integrations: [(await import('@playform/compress')).default()],
 });
